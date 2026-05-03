8bb8147178312141c5dbda5e60726a4ea153ebbc
kuzminpeta541-boop <kuzminpeta541@gmail.com>
2026-05-03 22:14:37 +0300

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,1 +1,33 @@
-Lampa.Noty.show('TEST');
+(function () {
+    'use strict';
+
+    if (!window.Lampa) return;
+
+    function openUakino() {
+        Lampa.Noty.show('UAKINO');
+
+        if (Lampa.Utils && Lampa.Utils.openUrl) {
+            Lampa.Utils.openUrl('https://uakino.best/ua/');
+        } else {
+            window.open('https://uakino.best/ua/', '_blank');
+        }
+    }
+
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') {
+
+            try {
+                Lampa.Controller.add('uakino', {
+                    title: 'UAKINO',
+                    icon: 'movie',
+                    onSelect: openUakino
+                });
+            }
+            catch (err) {
+                console.log('UAKINO plugin error:', err);
+            }
+
+        }
+    });
+
+})();
