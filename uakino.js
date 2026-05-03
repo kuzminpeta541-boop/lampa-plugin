555b1d0f82020b39fff1ec223d84943ea8be70ea
kuzminpeta541-boop <kuzminpeta541@gmail.com>
2026-05-03 22:34:56 +0300

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,42 +1,13 @@
 (function () {
     'use strict';
 
-    if (!window.Lampa) return;
+    console.log('UAKINO plugin loaded');
 
-    function openUakino() {
-        Lampa.Noty.show('Відкриваю UAKINO');
-
-        if (Lampa.Utils && Lampa.Utils.openUrl) {
-            Lampa.Utils.openUrl('https://uakino.best/ua/');
-        } else {
-            window.open('https://uakino.best/ua/', '_blank');
-        }
+    if (!window.Lampa) {
+        console.log('Lampa not found');
+        return;
     }
 
-    // чекаємо відкриття сторінки фільму
-    Lampa.Listener.follow('full', function (e) {
-
-        // важливо: саме complete (у деяких збірках пишеться complite)
-        if (e.type === 'complite' || e.type === 'complete') {
-
-            try {
-                // e.object = сторінка фільму
-                if (e.object && e.object.addButton) {
-
-                    e.object.addButton({
-                        title: 'UAKINO',
-                        icon: 'link',
-                        onSelect: openUakino
-                    });
-
-                } else {
-                    console.log('UAKINO: addButton не знайдено в цій версії Lampa');
-                }
-
-            } catch (err) {
-                console.log('UAKINO error:', err);
-            }
-        }
-    });
+    Lampa.Noty.show('UAKINO plugin OK');
 
 })();
