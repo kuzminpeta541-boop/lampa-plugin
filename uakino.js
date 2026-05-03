1602804842ff4214d06f46e0b7dc1a409406f87c
kuzminpeta541-boop <kuzminpeta541@gmail.com>
2026-05-03 21:46:41 +0300

    Add files via upload

---
+++ uakino.js
@@ -0,0 +1,22 @@
+﻿Lampa.Plugin.create({
+    name: 'UAKINO',
+    version: '1.0.0',
+    description: 'Фільми з UAKINO',
+
+    init: function () {
+        Lampa.Noty.show('UAKINO активовано');
+
+        function openSite() {
+            window.open('https://uakino.best/ua/', '_blank');
+        }
+
+        Lampa.Listener.follow('app', function(e){
+            if(e.type == 'ready'){
+                Lampa.Controller.add('uakino_btn', {
+                    icon: 'movie',
+                    onSelect: openSite
+                });
+            }
+        });
+    }
+});
